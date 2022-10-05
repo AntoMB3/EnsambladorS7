dfbf596d5905493325ea9e5f6a1cd2482590c284
Anto <antonio.munbar3@gmail.com>
2022-10-04 21:24:04 -0500

    con fecha

--- practica4.asm
+++ practica4.asm
@@ -14,6 +14,15 @@ MSG DB "$"
 HORARIO DB 15 DUP(?)
 DIVI DB 10 
 
+DIVS DW ?
+divis DB 10
+
+ANIO DB 4 DUP(?)
+MESe DB ?
+DIAe DB ?
+MES DB 2 DUP(?)
+DIA DB 2 DUP(?)
+
 
 ;Declaraciones (variables, constantes, etc...)
 
@@ -86,17 +95,65 @@ inicio:
          MOV DI,0
          CICLO:
             CMP DI,11
-            JA SALIR
+            JA obtenerFecha
             
             ADD HORARIO[DI],30h
             INC DI
             JMP CICLO
+            
+         obtenerFecha:
+            MOV AH,02Ah
+            INT 21h
+            MOV MESe,DH
+            MOV DIAe,DL
+            XOR DX,DX
+            JMP obtenerAnio
+            
+         obtenerAnio:
+            XOR DI, DI
+            MOV DIVS,1000
+            JMP LlenarVectorAnio
+         
+         LlenarVectorAnio:
+            MOV AX,CX
+            DIV DIVS
+            MOV ANIO[DI],AL
+            INC DI
+            MOV AX,DIVS
+            DIV divis
+            MOV DIVS,AX
+            MOV CX,DX
+            XOR DX,DX
+            
+            CMP DIVS,256
+            JE obtenerMes
+            JMP LlenarVectorAnio
+            
+         obtenerMes:
+            XOR AX,AX
+            XOR DX,DX
+            MOV AL,MESe
+            DIV divis
+            MOV DI,0
+            MOV MES[DI],AL
+            INC DI
+            MOV MES[DI],AH
+            
+         obtenerDia:
+            XOR AX,AX
+            XOR DX,DX
+            MOV AL,DIAe
+            DIV divis
+            MOV DI,0
+            MOV DIA[DI],AL
+            INC DI
+            MOV DIA[DI],AH
+            
+            
          
          SALIR:
          
-         MOV AH,09h
-         MOV DX, Offset hora
-         INT 21h
+         
          
          ;Terminador de programa
        MOV AH, 4Ch ;Llama al sercivio 09h de
